409d70067ff24221a9e8fe2475523e30bd2950d1
Brian Catraguna <briancatraguna@rocketmail.com>
2020-10-15 22:57:00 +0700

    saving default images

---
+++ Star Tracker/generating_data.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def displayImg(img,cmap=None):
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    fig = plt.figure(figsize=(12,10))
+    ax = fig.add_subplot(111)
+    ax.imshow(img,cmap)
+    plt.show()
+
+#Getting the raw images from the directory
+path = 'C:/Stellarium_Images/'
+dataset = []
+for filename in os.listdir(path):
+    img = cv2.imread(os.path.join(path,filename))
+    if img is not None:
+        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        new_img = gray_img[200:500,600:1000]
+        dataset.append(new_img)
+
+#Saving the default image to a directory
+for index,image in enumerate(dataset):
+    displayImg(image)
+    path = 'C:/PythonPrograms/GitClones/CodingProgressforThesis/Star Tracker/dataset/'+str(index)+'/'
+    file_name = str(index)+'_000.jpg'
+    cv2.imwrite(path+file_name,image)
