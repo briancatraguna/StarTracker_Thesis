933693a4c6aa98d97571479d34ace057bbc2d85c
Brian Catraguna <briancatraguna@rocketmail.com>
2020-10-31 14:16:52 +0700

    fixing errors and adding accuracy function

--- Star Tracker/evaluate.py
+++ Star Tracker/evaluate.py
@@ -6,9 +6,13 @@ model = load_model('C:/PythonPrograms/GitClones/StarTracker_Thesis/Star Tracker/
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.preprocessing import image
 
-def predict_class(path):
+def predict_class(class_no):
+    directory = 'C:/PythonPrograms/GitClones/StarTracker_Thesis/Star Tracker/dataset/test/'
+    class_no = str(class_no)+'/'
+    path = os.path.join(directory,class_no)
     predict_results = []
     for filename in os.listdir(path):
         test_image = image.load_img(
@@ -17,7 +21,18 @@ def predict_class(path):
             )
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image,axis=0)
-        result = cnn.predict(test_image)
+        result = model.predict(test_image)
+        result = result.reshape(8)
         predict_results.append(result)
     return predict_results
 
+def get_accuracy(predict_results,class_no):
+    right = 0
+    wrong = 0
+    for result in predict_results:
+        if result[class_no] == 1:
+            right+=1
+        else:
+            wrong+=1
+    accuracy = right/(right+wrong)
+    return accuracy
